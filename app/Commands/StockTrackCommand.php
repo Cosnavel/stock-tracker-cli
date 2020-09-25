b7674d8c2bf5ccc855c78ced4a67119fd7fb6330
Cosnavel <niclasdevelop@gmail.com>
2020-09-25 23:55:52 +0200

    style ci

--- app/Commands/StockTrackCommand.php
+++ app/Commands/StockTrackCommand.php
@@ -34,7 +34,8 @@ class StockTrackCommand extends Command
     /**
      * Define the command's schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     *
      * @return void
      */
     public function schedule(Schedule $schedule): void
